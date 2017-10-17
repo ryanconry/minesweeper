b51e1bdcb145ada97c2c42e4f9651159f32ec116
ryanconry <r.conry@hotmail.com>
2017-10-17 15:44:37 -0400

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,12 @@
+const blankLine = '  |   |  '
+console.log('This is what an empty board looks like');
+for(let i=0;i<3;i++){
+  console.log(blankLine);
+}
+
+console.log('This is what a game might look like: ')
+const guessLine='1 |   |  ';
+const bombLine='  | B |  ';
+console.log(blankLine);
+console.log(guessLine);
+console.log(bombLine);
